5adb3d013f393aa3ccd9d19a6978303018dbe907
fumba.me <fumba.me@gmail.com>
2017-04-21 15:46:47 -0400

    initial commit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
-import { browser, element, by } from 'protractor';
+import { PaintTheGlobeStravaPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('paint-the-globe-strava App', () => {
+  let page: PaintTheGlobeStravaPage;
 
-  let expectedMsg = 'Hello Angular';
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new PaintTheGlobeStravaPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
-
 });
